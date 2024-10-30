3601737e9cc3afdfce8645d41dd6fabb8c4dc905
Akshat Srivastava <akshatsrivastava625@gmail.com>
2024-10-30 21:59:52 +0530

    add bit programs

--- src/Bit/PowerOfTwo.java
+++ src/Bit/PowerOfTwo.java
@@ -6,10 +6,12 @@ public class PowerOfTwo {
         System.out.println(power(x));
     }
 
+
     private static boolean power(int x) {
         if(x==0)
             return false;
         return ((x & (x-1)) == 0);
+        //one line expression
+        // return (x!=0) && (x & (x-1) == 0);
     }
-
 }
